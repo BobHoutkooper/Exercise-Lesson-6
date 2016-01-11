f4dab98e3d5a0e08e1be5bdb6a5c5113f38fa830
BobHoutkooper <houtkooper.b@gmail.com>
2016-01-11 14:42:07 +0100

    Start-up(2)

---
+++ main.R
@@ -0,0 +1,20 @@
+# Loner
+# Bob Houtkooper
+# 08-01-2016
+
+# Make directories and set Working directory
+setwd("~/bin/Exercises/Exercise lesson 6")
+dir.create("Data")
+dir.create("R")
+dir.create("Output")
+
+# Import packages
+
+
+# Source functions
+
+
+# Download data
+
+
+# Load Datasets
